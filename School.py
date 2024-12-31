17b5b3ac23cdc40bad7845002a143d05d3f4c134
THoIBi4 <d.sagolsem9@gmail.com>
2024-12-31 17:58:18 +0530

    Create School.py

---
+++ School.py
@@ -0,0 +1,10 @@
+# simple program to check if a number is positive negative or 0
+number=input("enter a number:")
+#check
+if number.startswith("-"):
+  print("number is negative")
+elif number=="0":
+  print("number is 0")
+else:
+  print("number is positive")
+  # end
